119b1b0ac7aa0814f92e99c057439dfc2ab2fb5f
maximiloi <maximiloi@gmail.com>
2022-12-24 21:01:08 +0300

    feat: add mobile version

--- plants/src/js/files/script.js
+++ plants/src/js/files/script.js
@@ -14,12 +14,12 @@ const optionsContainer = document.querySelector('.options-container');
 const optionsList = document.querySelectorAll('.option');
 
 selected.addEventListener('click', () => {
-  optionsContainer.classList.toggle('active');
+    optionsContainer.classList.toggle('active');
 });
 
 optionsList.forEach((o) => {
-  o.addEventListener('click', () => {
-    selected.innerHTML = o.querySelector('label').innerHTML;
-    optionsContainer.classList.remove('active');
-  });
+    o.addEventListener('click', () => {
+        selected.innerHTML = o.querySelector('label').innerHTML;
+        optionsContainer.classList.remove('active');
+    });
 });
