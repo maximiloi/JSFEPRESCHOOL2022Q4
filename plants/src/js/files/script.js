5dc686745941ad89ce66270c8eac32e550435298
maximiloi <maximiloi@gmail.com>
2023-01-10 09:46:04 +0300

    feat: add js city selection

--- plants/src/js/files/script.js
+++ plants/src/js/files/script.js
@@ -2,33 +2,44 @@
 // import { isMobile } from './functions.js';
 // import { formsModules } from "./forms/forms.js";
 
-console.log('Ваша оценка - 79.5 баллов');
-// console.log('');
-// console.log('Отзыв по пунктам ТЗ:');
-// console.log('');
-// console.log('Выполненные пункты:');
+// console.log('Ваша оценка - 79.5 баллов');
 
-const selected = document.querySelector('.selected');
-const optionsContainer = document.querySelector('.options-container');
+const ADRESSES_LIST = {
+    canandaigua: ['Canandaigua, NY', '+1 585 393 0001', '151 Charlotte Street'],
+    newyorkcity: ['New York City', '+1 212 456 0002', '9 East 91st Street'],
+    yonkers: ['Yonkers, NY', '+1 914 678 0003', '511 Warburton Ave'],
+    sherrill: ['Sherrill, NY', '+1 315 908 0004', '14 WEST Noyes BLVD'],
+};
 
-const optionsList = document.querySelectorAll('.option');
+// Установка адреса в меню
+const setAdress = (city) => {
+    const adressBoxWrapper = document.querySelector('.adress-box');
+    const adressOutElem = adressBoxWrapper.querySelectorAll('.adress-box__item--out');
+    const adressButton = adressBoxWrapper.querySelector('.adress-box__button');
 
-selected.addEventListener('click', () => {
-    optionsContainer.classList.toggle('active');
-});
+    let count = 0;
 
-optionsList.forEach((o) => {
-    o.addEventListener('click', () => {
-        selected.innerHTML = o.querySelector('label').innerHTML;
-        optionsContainer.classList.remove('active');
-    });
-});
+    if (adressBoxWrapper.closest('.none')) {
+        adressBoxWrapper.classList.remove('none');
+        adressBoxWrapper.classList.add('active');
+    }
+
+    for (const key in ADRESSES_LIST) {
+        if (key === city) {
+            adressOutElem.forEach((value) => {
+                value.innerHTML = ADRESSES_LIST[key][count];
+                count++;
+            });
+            adressButton.href = `tel:${ADRESSES_LIST[key][1].split(' ').join('')}`;
+        }
+    }
+};
 
 const documentActions = (e) => {
     const targetElem = e.target;
     // console.log('%c [ targetElem ]-29', 'font-size:13px; background:pink; color:#bf2c9f;', targetElem);
-    // Работа с меню
 
+    // Работа с меню
     // открываем меню и закрываем меню
     if (targetElem.closest('.nav__burger')) {
         document.documentElement.classList.toggle('menu-open');
@@ -43,6 +54,34 @@ const documentActions = (e) => {
     if (!targetElem.closest('.nav__burger') && !targetElem.closest('.nav__list')) {
         document.documentElement.classList.remove('menu-open');
     }
+
+    // Работа с дропменю в контактах
+    if (targetElem.closest('.selected')) {
+        document.querySelector('.options-container').classList.toggle('active');
+    }
+
+    if (targetElem.closest('.options-container label')) {
+        document.querySelector('.selected').innerHTML = targetElem.innerHTML;
+        document.querySelector('.options-container').classList.remove('active');
+        setAdress(targetElem.dataset.city);
+    }
 };
 
 document.addEventListener('click', documentActions);
+
+// const selected = document.querySelector('.selected');
+// const optionsContainer = document.querySelector('.options-container');
+
+// selected.addEventListener('click', () => {
+//     optionsContainer.classList.toggle('active');
+// });
+
+// const optionsList = document.querySelectorAll('.option');
+
+// optionsList.forEach((o) => {
+//     o.addEventListener('click', () => {
+//         document.querySelector('.selected').innerHTML = o.querySelector('label').innerHTML;
+//         document.querySelector('.options-container').classList.remove('active');
+//         setAdress(o.querySelector('label').dataset.city);
+//     });
+// });
