9d9c488a2c2a55263147958a281ec2ff4dab80eb
maximiloi <maximiloi@gmail.com>
2023-01-31 10:23:09 +0300

    feat: add module adv-player part3

--- momentum/src/js/module/musicPlayer.js
+++ momentum/src/js/module/musicPlayer.js
@@ -5,21 +5,33 @@ const prevBtn = document.querySelector('.play-prev');
 const nextBtn = document.querySelector('.play-next');
 const playListOut = document.querySelector('.play-list');
 const trackNameOut = document.querySelector('.player-name');
-const trackTimeOut = document.querySelector('.player-time');
+const trackTimeOut = document.querySelector('.player-current-time');
 const trackTotalTimeOut = document.querySelector('.player-total-time');
-
-// .player-duration {
-// }
-// .player-progress {
-// }
+const seekSlider = document.querySelector('.player-duration');
+const volumeSlider = document.querySelector('.player-volume');
 
 let trackNumber = 0;
 const audio = new Audio();
 
 let isPlay = false;
 
+function calculateTime(secs) {
+    const minutes = Math.floor(secs / 60);
+    const seconds = Math.floor(secs % 60);
+    const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+    return `${minutes}:${returnedSeconds}`;
+}
+
+function displayDuration() {
+    trackTotalTimeOut.textContent = calculateTime(audio.duration);
+}
+
+function setSliderMax() {
+    seekSlider.max = Math.floor(audio.duration);
+}
+
 function generationPlaylist() {
-    let playListElem = playList.reduce((acum, item) => {
+    const playListElem = playList.reduce((acum, item) => {
         acum += `<li class="play-item">${item.title}</li>`;
         return acum;
     }, ``);
@@ -31,6 +43,7 @@ function trackActive(num) {
     const trackItems = document.querySelectorAll('.play-item');
     trackItems.forEach((item) => item.classList.remove('item-active'));
     trackItems[num].classList.add('item-active');
+    trackNameOut.textContent = trackItems[num].textContent;
 }
 
 function playAudio(num) {
@@ -87,6 +100,16 @@ function toggleBtn() {
 
 generationPlaylist();
 
+if (audio.readyState > 0) {
+    displayDuration();
+    setSliderMax();
+} else {
+    audio.addEventListener('loadedmetadata', () => {
+        displayDuration();
+        setSliderMax();
+    });
+}
+
 playBtn.addEventListener('click', () => {
     playAudio(trackNumber);
     toggleBtn();
@@ -98,6 +121,24 @@ nextBtn.addEventListener('click', playNext);
 
 audio.addEventListener('ended', () => {
     playNext();
-    playAudio(trackNumber);
     playBtn.classList.add('pause');
+});
+
+volumeSlider.addEventListener('input', (e) => {
+    // Управления громкостью
+    const value = e.target.value;
+    audio.volume = value / 100;
+});
+
+seekSlider.addEventListener('input', () => {
+    trackTimeOut.textContent = calculateTime(seekSlider.value);
+});
+
+seekSlider.addEventListener('change', () => {
+    audio.currentTime = seekSlider.value;
+});
+
+audio.addEventListener('timeupdate', () => {
+    seekSlider.value = Math.floor(audio.currentTime);
+    trackTimeOut.textContent = calculateTime(seekSlider.value);
 });
