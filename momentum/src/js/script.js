dc75955c9ea21f23f22fd536a5653b161156e822
maximiloi <maximiloi@gmail.com>
2023-02-27 15:47:26 +0300

    fix: init2

--- momentum/src/js/script.js
+++ momentum/src/js/script.js
@@ -3,23 +3,23 @@ import {} from './module/musicPlayer.js';
 import {} from './module/todo.js';
 import {} from './module/pomodoro.js';
 import {} from './module/setting.js';
-// import showTime from './module/date.js';
-// import {} from './module/weather.js';
-// import { getQuotes } from './module/quote.js';
-// import { lng, languageForQuote, showTranslationApi } from './module/translation.js';
+import showTime from './module/date.js';
+import {} from './module/weather.js';
+import { getQuotes } from './module/quote.js';
+import { lng, languageForQuote, showTranslationApi } from './module/translation.js';
 
 export function getRandomNum(min, max) {
     return Math.floor(Math.random() * max) + min;
 }
 
-// export function init() {
-//     showTime();
-//     generationPlaylist();
-//     getQuotes(lng(languageForQuote));
-//     showTranslationApi();
-// }
+export function init() {
+    showTime();
+    // generationPlaylist();
+    getQuotes(lng(languageForQuote));
+    showTranslationApi();
+}
 
-// init();
+init();
 
 console.log(
     'self-check: https://rolling-scopes-school.github.io/maximiloi-JSFEPRESCHOOL2022Q4/momentum/momentum/README.md'
